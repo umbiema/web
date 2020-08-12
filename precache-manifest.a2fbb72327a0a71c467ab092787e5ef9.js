41147ce4bbb7cad4220f9da3b22573190548ce3b
Ragil Bima <twinbiema@gmail.com>
2020-08-12 13:42:32 +0700

    Updates

---
+++ precache-manifest.a2fbb72327a0a71c467ab092787e5ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec005ce7f57d1d7757a7e41c5295d49",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "ce8a25b8625759ce1678",
+    "url": "/web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc87f939e4c26b366fb2",
+    "url": "/web/static/js/2.b0203e6a.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/web/static/js/2.b0203e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8a25b8625759ce1678",
+    "url": "/web/static/js/main.c0b8dd84.chunk.js"
+  },
+  {
+    "revision": "2e81f3457ebaed490abe",
+    "url": "/web/static/js/runtime-main.6e35b99d.js"
+  }
+]);
